cecd03d194d7f59b097cf6672fe7b8d8580e32f6
Charles Moulliard <ch007m@gmail.com>
2020-12-04 18:09:00 +0100

    Update hello.groovy

--- hello.groovy
+++ hello.groovy
@@ -1,7 +1,10 @@
 import jenkins.model.*
 
-node('master') {
+pipeline { 
+  agent any 
+  node('master') {
     stage('say-hello') {
         echo "Hello World from the jnlp-maven-client POD !"
     }
+  }
 }
