f254c2abcdbd108ef2d7e32b2e05bfe7db848f74
Charles Moulliard <ch007m@gmail.com>
2020-12-08 10:55:29 +0100

    Add a label to the jb created

--- jobs/hello_fs_job/job.groovy
+++ jobs/hello_fs_job/job.groovy
@@ -1,6 +1,7 @@
 freeStyleJob('hello-dsl-job') {
     displayName('hello-dsl-job')
     description('Job DSL to say hello.')
+    label('jnlp')
 
     steps {
         shell('Job DSL Saying Hello World !')
