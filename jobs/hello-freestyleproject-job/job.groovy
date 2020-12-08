fa37c837bcea6290d5500646b0eb780cfd200480
Charles Moulliard <ch007m@gmail.com>
2020-12-08 10:28:15 +0100

    Add freestyle project

---
+++ jobs/hello-freestyleproject-job/job.groovy
@@ -0,0 +1,10 @@
+
+freeStyleJob('hello-dsl-job') {
+    displayName('hello-dsl-job')
+    description('Job DSL to say hello.')
+
+    steps {
+        shell('Job DSL Saying Hello World !')
+    }
+
+}
